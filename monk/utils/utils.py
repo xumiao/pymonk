85e177fa3a05a352d4e0cd0b1770d781b52ba227
xu miao <pacificxumiao@gmail.com>
2014-03-09 13:58:06 -0700

    strip off useless functions

--- monk/utils/utils.py
+++ monk/utils/utils.py
@@ -7,19 +7,6 @@ Created on Mon Oct 21 10:44:50 2013
 
 import pickle
 import StringIO
-from twisted.python import log
-
-
-def GetOrDefault(a, key, default, convert=lambda x: x):
-    try:
-        r = a[key]
-        if type(r) == list:
-            return convert(r[0])
-        else:
-            return convert(r)
-    except Exception as e:
-        log.msg(e)
-        return default
 
 
 def Serialize(a):
@@ -44,7 +31,3 @@ def LowerFirst(astr):
         return astr[:1].lower() + astr[1:]
     else:
         return None
-
-
-def GetIds(objs):
-    map(lambda x: x._id, objs)
