10e9f384b2765dfadcb50b05a885c7cdbab7255d
xu miao <pacificxumiao@gmail.com>
2014-03-24 18:11:03 -0700

    move configuration to core, and add constants

---
+++ monk/core/configuration.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 16 23:42:45 2014
+
+@author: xm
+"""
+import yaml
+import socket
+
+class Configuration(object):
+
+    def __init__(self, configurationFileName):
+        self.uidConnectionString = 'localhost'
+        self.uidDataBaseName = 'uidDB'
+        self.modelConnectionString = 'localhost'
+        self.modelDataBaseName = 'TestMONKModel'
+        self.pandaCollectionName = 'PandaStore'
+        self.pandaFields = {'weights':-1}
+        self.turtleCollectionName = 'TurtleStore'
+        self.turtleFields = {}
+        self.viperCollectionName = 'ViperStore'
+        self.viperFields = {}
+        self.mantisCollectionName = 'MantisStore'
+        self.mantisFields = {'solvers':-1}
+        self.monkeyCollectionName = 'MonkeyStore'
+        self.monkeyFields = {}
+        self.tigressCollectionName = 'TigressStore'
+        self.tigressFields = {}
+        self.dataConnectionString = 'localhost'
+        self.dataDataBaseName = 'TestMONKData'
+        self.entityCollectionName = 'EntityStore'
+        self.entityFields = {} 
+        self.relationCollectionName = 'RelationStore'
+        self.relationFields = {}
+        self.logFileName = 'monk'
+        self.logLevel = 'logging.DEBUG'
+        self.kafkaConnectionString = "mozo.cloudapp.net:9092"
+        self.kafkaGroup = 'test'
+        self.kafkaTopic = 'test_topic'
+        self.kafkaPartitionId = 0
+        self.monkHost = socket.gethostbyname(socket.gethostname())
+        self.monkPort = 8887
+                
+        with open(configurationFileName, 'r') as conf:
+            self.__dict__.update(yaml.load(conf))
