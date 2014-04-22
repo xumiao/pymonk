056212c55a83f951fa633e42611d1706c560402a
xu miao <pacificxumiao@gmail.com>
2014-04-22 15:20:01 -0700

    unset timeout

--- monk/core/crane.py
+++ monk/core/crane.py
@@ -207,12 +207,7 @@ class Crane(object):
 
     def load_all_in_ids(self, query, skip, num):
         try:
-            logger.debug('begin retrieval')
-            cursor = self._coll.find(query, {'_id': 1}, skip=0, limit=num)
-            results = list(cursor)
-            logger.debug('closing cursor')
-            cursor.close()
-            return results
+            return list(self._coll.find(query, {'_id': 1}, skip=0, limit=num))
         except Exception as e:
             logger.warning(e.message)
             logger.warning('can not load documents by query'.format(query))
